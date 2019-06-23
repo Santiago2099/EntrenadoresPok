7d7e8890f8bf186ea8bf01853adb09ed8a29dc89
Santiago David Rincon Castillo <santiago080699@gmail.com>
2019-06-22 21:04:48 -0500

    instalado correctamente

---
+++ resources/views/trainers/Show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Trainer')
+
+@section('content')
+	<img style="height: 200px; width: 200px; background-color: #EFEFEF; margin: 20px" class="card-img-top rounded-circle mx-auto d-block" src="/images/{{$trainer->avatar}}" alt="">
+	<div class="text-center">
+		<h5 class="card-title">{{$trainer->name}}</h5>
+		<p>Los entrenadores Pokémon (Pokémon Trainer en inglés) son personas que se encargan de capturar, criar, entrenar y cuidar de dichas criaturas. Sin embargo, cualquier persona que posea un Pokémon puede considerarse entrenador, por lo tanto, un coordinador Pokémon es también lo primero. El nombre del entrenador original de cada Pokémon siempre se mantiene, aunque este se intercambie, y puede consultarse en su menú de datos, estando representado por las siglas EO (Entrenador Original).</p>
+		<a href="/trainers/{{$trainer->slug}}/edit" class="btn btn-primary">Editar</a>
+	</div>
+@endsection
